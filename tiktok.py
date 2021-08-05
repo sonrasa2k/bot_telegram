2abf52732c6ab2b171b0c19e77eb0bb270ac0cd2
sonrasa2k <sonnguyen663615@gmail.com>
2021-08-05 23:50:48 +0700

    fix trending

--- tiktok.py
+++ tiktok.py
@@ -219,6 +219,7 @@ class Download:
                 headers={
                     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
                     "authority": "www.tiktok.com",
+                    "Accept-Language":"vi,en-US;q=0.9,en;q=0.8",
                     "Accept-Encoding": "gzip, deflate",
                     "Connection": "keep-alive",
                     "Host": "www.tiktok.com",
